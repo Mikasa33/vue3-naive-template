6e9455e16436369c1fad07bf134c7400bb68aa00
Mikasa33 <mikasa33@qq.com>
2022-04-27 14:22:33 +0800

    添加 prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100, // 每行最多多少个字符换行
+  semi: true, // 在每一条语句末尾添加分号
+  vueIndentScriptAndStyle: true,
+  singleQuote: true, // 使用单引号
+  trailingComma: 'all', // 对象末尾逗号
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'auto',
+}
