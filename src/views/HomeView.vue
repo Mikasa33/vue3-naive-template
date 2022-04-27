5b2fa07df865332c5c29ddfba157959b3152bae3
Mikasa33 <mikasa33@qq.com>
2022-04-27 17:55:27 +0800

    添加暗黑模式

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-  import TheWelcome from '/@/components/TheWelcome.vue';
+  const isDark = ref(false);
+
+  function handleClick() {
+    isDark.value = !isDark.value;
+    if (isDark.value) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home flex-center flex-col">
+    <h1 class="text-black dark:text-white">This is an home page</h1>
+    <div>
+      <div
+        class="flex-center mb-4 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-600 border-none cursor-pointer transition-base"
+        @click="handleClick"
+      >
+        <div class="mr-1 text-xl i-icon-park-outline:sun-one dark:i-icon-park-outline:moon" /> 切换模式
+      </div>
+    </div>
+  </div>
 </template>
