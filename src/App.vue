86960a4a9375760d51698b9a20e9b6bd3084fae0
Mikasa33 <mikasa33@qq.com>
2022-04-28 13:30:55 +0800

    添加切换暗黑模式组件

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
 
 <template>
   <div class="flex-center flex-col h-100vh bg-light-50 dark:bg-dark-500">
+    <div>
+      <ToggleDark class="text-orange-500 cursor-pointer" />
+    </div>
+
     <nav>
       <RouterLink to="/" class="p-4 text-blue-800 dark:text-blue-400">Home</RouterLink>
       <RouterLink to="/about" class="p-4 text-blue-800 dark:text-blue-400">About</RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,5 @@
-<script setup lang="ts">
-  const isDark = ref(false);
-
-  function handleClick() {
-    isDark.value = !isDark.value;
-    if (isDark.value) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }
-</script>
-
 <template>
   <div class="home flex-center flex-col">
     <h1 class="text-black dark:text-white">This is an home page</h1>
-    <div>
-      <div
-        class="flex-center mb-4 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-600 border-none cursor-pointer transition-base"
-        @click="handleClick"
-      >
-        <div class="mr-1 text-xl i-icon-park-outline:sun-one dark:i-icon-park-outline:moon" /> 切换模式
-      </div>
-    </div>
   </div>
 </template>
