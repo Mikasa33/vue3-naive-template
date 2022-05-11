2ca3d1a2c573212ffe0a755d56c4f3935ea7e88f
Mikasa33 <mikasa33@qq.com>
2022-05-11 19:27:16 +0800

    修改引入

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import 'uno.css';
 import '/@/styles/global.less';
 
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 import App from './App.vue';
-import router from './router';
+import { setupRouter } from '/@/router';
+import { setupStore } from '/@/store';
 
 const app = createApp(App);
 
-app.use(createPinia());
-app.use(router);
+setupRouter(app);
+setupStore(app);
 
 app.mount('#app');
