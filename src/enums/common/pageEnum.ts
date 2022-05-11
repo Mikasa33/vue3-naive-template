039fa1af1f2bf07cdbed2c51b734be72b7f3468b
Mikasa33 <mikasa33@qq.com>
2022-05-11 19:25:37 +0800

    添加枚举值

---
+++ src/enums/common/pageEnum.ts
@@ -0,0 +1,8 @@
+export enum PageEnum {
+  // 登录页面地址
+  BASE_LOGIN = '/login',
+  // 首页地址
+  BASE_HOME = '/dashboard',
+  // 错误页面地址
+  ERROR_PAGE = '/exception',
+}
---
+++ src/enums/common/storageEnum.ts
@@ -0,0 +1,10 @@
+export enum storageEnum {
+  // 主题颜色
+  'theme-color' = '__THEME_COLOR__',
+  // 用户 token
+  'token' = '__TOKEN__',
+  // 用户刷新token
+  'refresh-token' = '__REFRESH_TOKEN__',
+  // 用户信息
+  'user-info' = '__USER_INFO__',
+}
