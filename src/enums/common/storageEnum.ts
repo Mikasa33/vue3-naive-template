456df6e2d699c120829b684b47deb75c6ced4ce4
Mikasa33 <mikasa33@qq.com>
2022-05-16 17:10:11 +0800

    格式化

--- src/enums/common/storageEnum.ts
+++ src/enums/common/storageEnum.ts
@@ -3,7 +3,7 @@ export enum storageEnum {
   'theme-color' = '__THEME_COLOR__',
   // 用户 token
   'token' = '__TOKEN__',
-  // 用户刷新token
+  // 用户刷新 token
   'refresh-token' = '__REFRESH_TOKEN__',
   // 用户信息
   'user-info' = '__USER_INFO__',
