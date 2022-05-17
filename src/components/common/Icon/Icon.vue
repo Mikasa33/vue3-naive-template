d2aad9889e4f65c21efa8648836de7ee7c8bbdf4
Mikasa33 <mikasa33@qq.com>
2022-05-17 10:58:05 +0800

    修改icon组件

---
+++ src/components/common/Icon/Icon.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { isNumber } from '/@/utils/is';
+
+  const props = withDefaults(defineProps<{
+    icon: string;
+    dark?: string;
+    size?: 'small' | 'medium' | 'large' | number;
+  }>(), {
+    size: 'medium',
+  });
+
+  const size = computed(() => {
+    const { size } = props;
+    if (isNumber(size)) {
+      return `text-${size}px`;
+    }
+    switch(size) {
+      case 'small': return 'text-xs';
+      case 'large': return 'text-lg';
+      default: return '';
+    }
+  });
+</script>
+
+<template>
+  <div :class="[icon, dark, size]" />
+</template>
